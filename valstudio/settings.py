642e77083a3cef6bf8941d71197b191cfac22b71
Aprendiz Informatica <apinf@SERVIASESORIA.LOCAL>
2025-08-08 13:51:07 -0500

    Fix collectstatic and staticfiles config

--- valstudio/settings.py
+++ valstudio/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,7 +52,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware', 
+    'whitenoise.middleware.WhiteNoiseMiddleware', # linea para que render cargue estilos
 ]
 
 ROOT_URLCONF = 'valstudio.urls'
@@ -119,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Carpeta de archivos estáticos recolectados
 STATICFILES_DIRS = [BASE_DIR / 'theme' / 'static']
 
 
